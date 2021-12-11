074b539bbb711f9d30f8c811ff8a899544bbdc4e
adds39939 <adamchandlercaunce@gmail.com>
2021-12-11 15:44:45 +0000

    Changed stuff

---
+++ adacaun.dev/adacaun.dev.Web/Components/UI/UIComponentBase.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Components;
+
+namespace adacaun.dev.Web.Components.UI;
+
+public class UIComponentBase : ComponentBase
+{
+    protected string FormatCss(string css)
+        => Regex.Replace(css, @"\s+", " ").Trim();
+}
